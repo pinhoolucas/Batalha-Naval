80258367ff74fb6a11faf0fd6519b4ebfb6013ba
Pinho <pinhopaeslucas@gmail.com>
2025-12-30 12:42:13 +0000

    colunas do tabuleiro

--- batalhanaval.c
+++ batalhanaval.c
@@ -11,6 +11,7 @@
 int main ()
 {
     int linhas[10] = {0,1,2,3,4,5,6,7,8,9};
+    char colunas[10] = {'A','B','C','D','E','F','G','H','I','J'};
 
     //Criação do Tabuleiro Vazio
     int tabuleiro[10][10] = {
@@ -31,6 +32,17 @@ int main ()
     //Imprimir o tabuleiro na tela 
     for(int i=0; i<10; i++) // i == imprimir as linhas
     {
+        
+        if(i==0)
+        {
+            printf("\t  ");
+            for(int i=0; i<10; i++)
+            {
+                printf(" %c", colunas[i]);
+            }
+            printf("\n\n");
+        }
+        
         printf("\t%d  ", linhas[i]); //imprime o numero da linha ao lado do tabuleiro
         
         for(int j=0; j<10; j++) // j == imprimir as colunas
