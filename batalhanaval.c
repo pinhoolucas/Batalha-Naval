8d43a687a624523786e16fd689e9b62f5884f6b8
Pinho <pinhopaeslucas@gmail.com>
2025-12-31 11:04:11 +0000

    Final Commit

--- batalhanaval.c
+++ batalhanaval.c
@@ -1,11 +1,14 @@
+//transformar os navios em vetores unidimensionais 
+
 #include <stdio.h>
 
 int main ()
 {
     int linhas[10] = {0,1,2,3,4,5,6,7,8,9};
     char colunas[10] = {'A','B','C','D','E','F','G','H','I','J'};
-    int navio_vertical;
-    int navio_horizontal;
+    int navio_vertical[3] = {3,3,3}, linha_v = 5, coluna_v = 5;
+    int navio_horizontal[3] = {3,3,3}, linha_h = 2, coluna_h  = 2;
+    int sobreposicao=0;
 
     //Criação do Tabuleiro Vazio, zero = água
     int tabuleiro[10][10] = {
@@ -23,36 +26,37 @@ int main ()
 
                              };
     
-    //Posicionamento do navio horizontal
-    for(navio_horizontal=0; navio_horizontal<3; navio_horizontal++)
+    //Navio Vertical
+    for(int i=0; i<3; i++)
     {
-        tabuleiro[2][1+navio_horizontal]=tabuleiro[2][1+navio_horizontal]+3;
-        //A soma dentro do [] define a posição, enquanto a soma fora, aumenta o valor de zero para 3.
-    }
-    //Posicionamento do navio vertical
-    for(navio_vertical=0; navio_vertical<3; navio_vertical++)
-    {
-        tabuleiro[5+navio_vertical][8]=tabuleiro[5+navio_vertical][8]+3;
-        //A soma dentro do [] define a posição, enquanto a soma fora, aumenta o valor de zero para 3.
-    }
-
-    //Condição de sobreposição de navios
-    for(int a=0; a<10; a++)
-    {
-        for(int b=0; b<10; b++)
+        if(tabuleiro[linha_v+i][coluna_v]!=0)
         {
-            if (tabuleiro[a][b]==6)
-            {
-                printf("\nErro: Houve sobreposicao de navios!\n");
-            }
+            sobreposicao++;
         }
+        tabuleiro[linha_v+i][coluna_v]=navio_vertical[i];
     }
     
+    //Navio Horizontal
+    for(int i=0; i<3; i++)
+    {
+        if(tabuleiro[linha_h][coluna_h+i])
+        {
+            sobreposicao++;
+        }
+        tabuleiro[linha_h][coluna_h+i]=navio_vertical[i];
+    }
+    
+    //Sobreposição dos navios
+    if(sobreposicao!=0)
+    {
+        printf("\nERRO: SOBREPOSICAO DE NAVAVIOS\n\n");
+    }
     
     //Imprimir o tabuleiro na tela 
-    for(int i=0; i<10; i++) // i == imprimir as linhas
-    {        
-        if(i==0) // Imprimir Colunas de A-J na parte superior do tabuleiro
+    for(int i=0; i<10; i++) 
+    {   
+        // Imprimir Colunas de A-J na parte superior do tabuleiro
+        if(i==0) 
         {
             printf("\t   ");
             for(int i=0; i<10; i++)
@@ -62,14 +66,15 @@ int main ()
             printf("\n\n");
         }
         
-        printf("\t%d   ", linhas[i]); //imprime o numero da linha ao lado do tabuleiro
+        //imprime o numero da linha ao lado do tabuleiro
+        printf("\t%d   ", linhas[i]); 
         
-        for(int j=0; j<10; j++) // j == imprimir as colunas
+        for(int j=0; j<10; j++)
         {
             printf("%d ", tabuleiro[i][j]);
         }
         
-        printf("\n"); //Quando termina de imrpimir uma linha, pula pra linha de baixo
+        printf("\n");
         
     }
 
